6797425dbf8ddb18353604a3175ed9849d6f07a6
Daisuke Sato <daisuke.sato1991@gmail.com>
2014-11-06 18:44:40 +0900

    create env Serverspec + Chef Solo

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'knife-solo'
+gem 'serverspec'
+gem 'rake'
+gem 'guard'
+gem 'guard-rspec'
 
--- site-cookbooks/httpd/recipes/default.rb
+++ site-cookbooks/httpd/recipes/default.rb
@@ -7,6 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
+#install package
 package "httpd" do
 	action :install
 end
+
+#start httpd
+service "httpd" do
+	action [:enable,:start]
+end
+
+#template the setting file
+template "httpd.conf" do
+	path "/etc/httpd/httpd.conf"
+	owner "root"
+	group "root"
+	mode 0644
+	#reload when the file updated
+	notifies :reload, 'service[httpd]'
+end
+
